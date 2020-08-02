2b989a6f99d8583e16b1cdd9a74abaab3dd6e2c5
HarithaRugada <haritharugada@gmail.com>
2020-08-02 15:34:28 +0530

    ADDED : Employee wage using switch case

--- empWage.js
+++ empWage.js
@@ -8,20 +8,21 @@ let empHours = 0;
 let empWage = 0;
 
 let empCheck = Math.floor(Math.random() * 10) % 3;
-if (empCheck == IS_PART_TIME)
+switch (empCheck)
 {
-	console.log("Employee is Present for part-time");
-	empHours = 4;
-}
-else if (empCheck == IS_FULL_TIME)
-{
-	console.log("Employee is Present for full time");
-	empHours = 8;
-}
-else
-{
-    	console.log("Employee is Absent");
-	empHours = 0;
+	case IS_PART_TIME:
+		console.log("Employee is Present for part-time");
+		empHours = 4;
+		break;
+
+	case IS_FULL_TIME:
+		console.log("Employee is Present for full time");
+		empHours = 8;
+		break;
+
+	default:
+	    	console.log("Employee is Absent");
+		empHours = 0;
 }
 
 empWage = empHours * EMP_RATE_PER_HOUR;
