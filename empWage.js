82eb66fd2e5a7e9592753d8befda55b1320414d1
HarithaRugada <haritharugada@gmail.com>
2020-08-02 15:29:42 +0530

    ADDED : Part time employee Wage

--- empWage.js
+++ empWage.js
@@ -1,15 +1,21 @@
 console.log("Welcome to Employee Wage Computation");
 
-let IS_PRESENT = 1;
+let IS_PART_TIME = 1;
+let IS_FULL_TIME = 2;
 let EMP_RATE_PER_HOUR = 25;
 
 let empHours = 0;
 let empWage = 0;
 
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == IS_PRESENT)
+let empCheck = Math.floor(Math.random() * 10) % 3;
+if (empCheck == IS_PART_TIME)
 {
-	console.log("Employee is Present");
+	console.log("Employee is Present for part-time");
+	empHours = 4;
+}
+else if (empCheck == IS_FULL_TIME)
+{
+	console.log("Employee is Present for full time");
 	empHours = 8;
 }
 else
